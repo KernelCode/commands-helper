91b421e7efcb79f3cb01f5f55ab0ca53c293a16e
Abdullah Al-taheri <abdullahaltahery@gmail.com>
2024-02-09 10:25:58 +0300

    update to Google Generative AI (Gimini)

--- README.md
+++ README.md
@@ -1,14 +1,8 @@
-# Google <a href="https://bard.google.com/"><img src="https://camo.githubusercontent.com/adb54264fe2ad5067d07d0752fc32600b4e6250073b01ce8c386575b431e3f06/68747470733a2f2f7777772e677374617469632e636f6d2f6c616d64612f696d616765732f66617669636f6e5f76315f31353031363063646466663766323934636533302e737667" height="20px"></a> Bard Command Line Helper
+# Google Generative AI (Gimini) cli helper .
 
 <br>
 
-> A package help using linux cli with the help of [Google Bard](https://bard.google.com/) through API
-
-<br>
-
-Never expose the `__Secure-1PSID` for your safety.
-
-> Note that while I referred to `__Secure-1PSID` value as an API KEY for convenience, it is not an officially provided API KEY.
+> A package help you getting ai suggestions using Google Gimini
 
 <br>
 
@@ -18,15 +12,9 @@ The latest stable release can be installed from NPM :
 
 ```
 npm install -g commands-helper
+# then set your api key generate from https://makersuite.google.com/app/apikey
+commands-helper --set-api-key="YOUR_API_KEY"
 ```
-
-<br>
-
-## Authentication
-
-1. Visit https://bard.google.com/
-2. F12 for console
-3. Session: Application → Cookies → Copy the value of `__Secure-1PSID` cookie.
 
 <br>
 
@@ -54,15 +42,9 @@ Sincerely grateful for any reports on new features or bugs. Your valuable feedba
 
 ## Contacts
 
-- Core maintainer: [Abdullah Al-tahery, Sana'a Yemen](https://github.com/kernelcode) <br>
+- Core maintainer: [Abdullah Al-taheri, Sana'a Yemen](https://github.com/kernelcode) <br>
 - E-mail: abdullah.altahery@gmail.com <br>
 
 ## Reference
 
 [1] https://github.com/kernelcode/commands-helper
-
-### Important Warning: All legal responsibilities associated with the use of the package lie with the user.
-
-This NodeJS package just provides code for JS developers to easily access Google Bard. it should be noted that there is no liability for the use of the code. Please refer to the Google Bard Official Document for more details.
-
-Thank you for your interest.
--- src/cli.js
+++ src/cli.js
@@ -1,40 +1,64 @@
 #!/usr/bin/env node
-const { formatJSONText } = require("./helpers");
-const args = require("args-parser")(process.argv);
-const Bard = new (require("./bard.ai.core"))(args);
-const fs = require("fs");
-const q = args.q;
 
+const args = require("args-parser")(process.argv);
+
+const fs = require("fs");
+const os = require("os");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const q = args.q;
+if (args["set-api-key"]) {
+  fs.writeFileSync(".api-key", args["set-api-key"]);
+  console.log("API Key saved!");
+  process.exit(0);
+}
 if (!q) {
   console.log('using : commands-helper --q="list all files in root dir?"');
-  process.exit();
+  process.exit(0);
 }
-Bard.getAnswer(
-  `i want you to only respond to me ONLY in json format when i ask you the response will be like this 
-  what the command to create a folder ?
-  your respons will be : {command:"mkdir YOUR_DIR_NAME"}
-  
-  start now :
-  my question is : ${q}?`
-)
-  .then((res) => {
-    if (res.content.includes("Response Error")) {
-      fs.unlinkSync(".token");
-      console.log("Your Google Bard token is invaled!");
-      console.log("\x1b[1m%s\x1b[0m", 'commands-helper --set-token="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"');
-      process.exit(1);
-    }
-    const command = formatJSONText(res.content + "");
-    console.log("Use command : ");
-    console.log("\x1b[1m%s\x1b[0m", "- " + command);
-
-    if (!command === formatJSONText(res.choices[0].content + ""))
-      console.log("- ", formatJSONText(res.choices[0].content + ""));
-    if (!command === formatJSONText(res.choices[1].content + ""))
-      console.log("- ", formatJSONText(res.choices[1].content + ""));
-    if (!command === formatJSONText(res.choices[2].content + ""))
-      console.log("- ", formatJSONText(res.choices[2].content + ""));
-  })
-  .catch((err) => {
-    console.log(err);
+function readApiKey(apikey) {
+  if (apikey) {
+    fs.writeFileSync(".api-key", apikey);
+    return apikey;
+  }
+  if (!fs.existsSync(".api-key")) {
+    console.log("Set Your Google Generative API_KEY using");
+    console.log("\x1b[1m%s\x1b[0m", 'commands-helper --set-api-key="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"');
+    process.exit(0);
+  }
+  return fs.readFileSync(".api-key", {
+    encoding: "utf-8",
   });
+}
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(readApiKey());
+
+async function run() {
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+  const prompt =
+    "Write 3 options separated by lines without any additional text or lines. The OS is: " +
+    os.platform() +
+    " Explain the command with a few words using this format: [command] # explaining command." +
+    " And the CLI command to do: " +
+    args.q;
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+
+  const lines = text.split("\n");
+  const dimColorEscapeCode = "\x1b[2m";
+  const resetEscapeCode = "\x1b[0m";
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim().split("#");
+    if (line) lines[i] = line[0] + dimColorEscapeCode + "#" + line[1] + resetEscapeCode;
+  }
+
+  const formattedText = lines.join("\n");
+  console.log(formattedText);
+}
+
+run();
